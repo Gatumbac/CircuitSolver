5edf1c5022feef37837a16d22deb6dfaff0c7eb6
Gabriel Tumbaco <gatumbac@espol.edu.ec>
2025-05-30 08:56:14 -0500

    Voltaje, alertas e imagen fija

--- js/app.js
+++ js/app.js
@@ -6,7 +6,21 @@ resistances.forEach(resistance => {
     })
 })
 
+const volt = document.querySelector("#volt");
+volt.addEventListener('input', (e) => {
+    const value = document.querySelector("#vval");
+    value.textContent = e.target.value + " (V)";
+})
+
 const form = document.querySelector("#form");
 form.addEventListener('submit', (e) => {
     e.preventDefault();
+    Swal.fire({
+        title: "Resultado: 5000(A)",
+        text: "Corriente Total Calculada!",
+        imageUrl: "../img/ampere1.png",
+        imageWidth: 200,
+        imageHeight: 200,
+        imageAlt: "Imagen de corriente"
+    });
 })
